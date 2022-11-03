5dc0b1728c1178627f9fd4ceb15f59b89dccbb70
RafalTaraska <paulo.dorosz@gmail.com>
2022-11-03 23:54:50 +0100

    step_1 start...exit

---
+++ magazyn.py
@@ -0,0 +1,13 @@
+
+print("1: Przyjęcie towaru")
+print("2: Wydanie towaru")
+
+operation = input("Co chcesz zrobić ? ")
+
+while True:
+    if operation == 'exit':
+        print("Koniec operacji...BYE!!!")
+        break
+    else:
+        operation = input("Co chcesz zrobić ? ")
+    continue
