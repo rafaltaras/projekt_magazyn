d4b116cb16ab62ad135e22c46781a14ff5de9c2a
RafalTaraska <paulo.dorosz@gmail.com>
2022-11-08 22:27:23 +0100

    step_2... show naglowek

--- magazyn.py
+++ magazyn.py
@@ -1,13 +1,25 @@
 
-print("1: Przyjęcie towaru")
-print("2: Wydanie towaru")
 
-operation = input("Co chcesz zrobić ? ")
+items = {
+    "Name": ['Cebula','Marchew','Pietruszka'],
+    "Quantity": [40,50,20],
+    "Unit": ['kg','kg','kg'],
+    "Unit Price (PLN)": [2.3, 4.1, 5]
+    }
+
+
+def get_items(towar):
+    naglowek = ""
+    for item in towar:
+        naglowek += item + '\t'
+    return naglowek
 
 while True:
+    operation = input("Please enter search criteria, or type 'exit' to exit the program: ")
     if operation == 'exit':
         print("Koniec operacji...BYE!!!")
         break
-    else:
-        operation = input("Co chcesz zrobić ? ")
-    continue
+    elif operation == "show":
+        results = get_items(items)
+        print(results)
+
