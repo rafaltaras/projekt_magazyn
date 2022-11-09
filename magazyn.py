fc2aa3ebc9bfa270745c28f4bf13136e723f489d
RafalTaraska <paulo.dorosz@gmail.com>
2022-11-09 23:34:33 +0100

    added column

--- magazyn.py
+++ magazyn.py
@@ -7,21 +7,24 @@ items = {
     "Unit Price (PLN)": [2.3, 4.1, 5]
     }
 
+def get_items(product):
+    headers = ""
+    for item in product:
+        headers += item + '\t\t'
+    print(headers)
 
-def get_items(towar):
-    naglowek = ""
-    for item in towar:
-        naglowek += item + '\t'
-    return naglowek
-
-
+    for i in range(0, len(product.values())-1):
+        for key in product.keys():
+            print(product[key][i], end='\t\t')
+        print('')
+       
 while True:
+    print('')
     operation = input("Please enter 'show', or type 'exit' to exit the program: ")
     if operation == 'exit':
         print("Koniec operacji...BYE!!!")
         break
     elif operation == "show":
         results = get_items(items)
-        print(results)
-        print('----''\t''-------''\t')
+        
 
