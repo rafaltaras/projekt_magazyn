0e39232298a58d24f411f9e3fff72041349b089a
RafalTaraska <paulo.dorosz@gmail.com>
2022-11-13 18:31:44 +0100

    added add new product

--- magazyn.py
+++ magazyn.py
@@ -18,12 +18,28 @@ def get_items(product):
             print(f"|{product[key][i]:10}|", end='\t')
         print('')
 
-def add_items():
-        name = input('Podaj nazwę towaru: ')
-        unit_name = ('Podaj jednostkę np. kg, l ?: ')
-        quantity = ('Podaj ilość: ')
-        unit_price = ('Podaj cenę jednostkową: ')
+def add_items(name, unit_name, quantity, unit_price):
+    for key, value in items.items():
+        if key == "Name":
+            value.append(name)
+        elif key == "Quantity":
+            value.append(quantity)
+        elif key == "Unit":
+            value.append(unit_name)
+        elif key == "Unit Price (PLN)":
+            value.append(unit_price)
+    print("")
+    print('Obency stan magazynu')
+    print('')
+    headers = ""
+    for item in items:
+         headers += f"|{item:10}|" + '\t'
+    print(headers)
 
+    for i in range(0, len(items.values())):
+        for key in items.keys():
+             print(f"|{items[key][i]:10}|", end='\t')
+        print('')
        
 while True:
     print('')
@@ -34,4 +50,10 @@ while True:
     elif operation == "show":
         results = get_items(items)
     elif operation == "add":
-        results = add_items()
+        name = input('Podaj nazwę towaru: ')
+        unit_name = input('Podaj jednostkę np. kg, l ?: ')
+        quantity = int(input('Podaj ilość: '))
+        unit_price = float(input('Podaj cenę jednostkową: '))
+        results = add_items(name, unit_name, quantity, unit_price)
+        
+
